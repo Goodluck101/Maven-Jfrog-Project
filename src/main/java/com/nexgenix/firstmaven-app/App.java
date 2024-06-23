de516296fbf40fa17918ab3b169ae463a5a0d940
Goodluck <ekeomaadiole@gmail.com>
2024-06-23 19:11:11 +0100

    committing my maven project

---
+++ src/main/java/com/nexgenix/firstmaven-app/App.java
@@ -0,0 +1,13 @@
+package com.nexgenix.firstmaven_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!. This my first version of the code" );
+    }
+}
